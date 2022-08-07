17f60ccdd7e705d912f519596e7962eca4bf7b1d
hiren <72260974+hirensavani-99@users.noreply.github.com>
2022-08-08 01:16:51 +0200

    first commit

---
+++ 5th.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleopen() {
+    console.log('hi')
+    this.classList.toggle('open');
+
+}
+
+function toggleactive(e) {
+    console.log(e.propertyName)
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+
+}
+
+
+panels.forEach(panel => panel.addEventListener('click', toggleopen));
+
+panels.forEach(panel => panel.addEventListener('transitionend', toggleactive));
